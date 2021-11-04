4754a3ed55f14e18faacb323ab999c37652b0435
tdhhxn <tdhhxn@umsystem.edu>
2021-11-04 13:09:35 -0500

    Initial Menu

---
+++ src/container/Menu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Menu.css"
+
+const Menu = () =>{
+    return(
+        <div>
+           <div id="title">
+               Flaming Arrow
+           </div>
+        </div>
+    )
+}
+
+export default Menu
