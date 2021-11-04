bf79fb81a604d4aa3d45db73912b02dbfaaeb409
tdhhxn <tdhhxn@umsystem.edu>
2021-11-04 14:01:18 -0500

    Finish first set up on menu

--- src/container/Menu.jsx
+++ src/container/Menu.jsx
@@ -7,6 +7,17 @@ const Menu = () =>{
            <div id="title">
                Flaming Arrow
            </div>
+           <div className="menu">
+               <button className="menu-button">
+                   <span class="text-button">Start Game</span>
+                </button>
+                <button className="menu-button">
+                   <span class="text-button">Instruction</span>
+                </button>
+                <button className="menu-button">
+                   <span class="text-button">History</span>
+                </button>
+           </div>
         </div>
     )
 }
